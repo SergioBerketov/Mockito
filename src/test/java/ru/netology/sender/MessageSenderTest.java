b706c35d761e3f001eec99e907b6d3ce14254df2
Sergei Berketov <sergio_berketov@mail.ru>
2023-06-25 23:21:49 +0700

    tests added

---
+++ src/test/java/ru/netology/sender/MessageSenderTest.java
@@ -0,0 +1,59 @@
+package ru.netology.sender;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
+import ru.netology.i18n.LocalizationService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageSenderTest {
+
+    private MessageSender messageSender;
+    private Map<String, String> headers;
+    private GeoService geoService;
+    private LocalizationService localizationService;
+
+
+    @BeforeEach
+    void settings() {
+
+        geoService = Mockito.mock(GeoService.class);
+        localizationService = Mockito.mock(LocalizationService.class);
+        headers = new HashMap<>();
+        messageSender = new MessageSenderImpl(geoService, localizationService);
+    }
+
+    @Test
+    public void testSendRussiaMessage() {
+
+        Mockito.when(geoService.byIp("172.")).thenReturn(new Location(null, Country.RUSSIA, null, 0));
+        Mockito.when(localizationService.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
+
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.");
+        String actual = messageSender.send(headers);
+        String expected = localizationService.locale(Country.RUSSIA);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSendUSAMessage() {
+
+        Mockito.when(geoService.byIp("96.")).thenReturn(new Location(null, Country.USA, null, 0));
+        Mockito.when(localizationService.locale(Country.USA)).thenReturn("Welcome");
+
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.");
+        String actual = messageSender.send(headers);
+        String expected = localizationService.locale(Country.USA);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
